9c04335bf28f6dcca01ffed921f7cdaa63522b0c
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-08 21:55:34 +0000

    Update Day 2 Operators.py

---
+++ Day 2 Operators.py
@@ -0,0 +1,31 @@
+# Object: 30 Days of Code HackerRank
+# Author: Nazmul Hassan
+# Language: Python
+# Date: 08/01/2021
+
+# Day 2
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+
+
+def solve(meal_cost, tip_percent, tax_percent):
+
+    totalCost = meal_cost + meal_cost * \
+        tip_percent / 100 + meal_cost * tax_percent/100
+    print(round(totalCost))
+
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
